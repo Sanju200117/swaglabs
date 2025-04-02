10d16637a8aad798e95735672ef53745b6ee8807
T S <sts@galaxe.com>
2025-04-02 15:29:47 +0530

    After commenting

--- cypress/e2e/SWAGLABS.cy.js
+++ cypress/e2e/SWAGLABS.cy.js
@@ -1,63 +1,63 @@
-describe('Login', () => {
-  it('Visit_website', () => {
+// describe('Login', () => {
+//   it('Visit_website', () => {
   
-  //Visit the site
+//   //Visit the site
 
-    cy.visit("https://www.saucedemo.com");
+//     cy.visit("https://www.saucedemo.com");
 
-  //Login
+//   //Login
 
-    cy.fixture('swaglabs').then( (testdata)=>{
+//     cy.fixture('swaglabs').then( (testdata)=>{
 
-      //Using custom command for login and also fixture file for taking username and password
-      cy.login(testdata.username, testdata.password);
+//       //Using custom command for login and also fixture file for taking username and password
+//       cy.login(testdata.username, testdata.password);
       
 
-      // cy.get('#user-name').type(testdata.username);
-      // cy.get('#password').type(testdata.password);
-      // cy.get('#login-button').click();
+//       // cy.get('#user-name').type(testdata.username);
+//       // cy.get('#password').type(testdata.password);
+//       // cy.get('#login-button').click();
 
-    });
+//     });
     
-    // let usernames = ["standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user"]
-    // let selectedUsername = usernames[0];
-    // console.log("Selected username: ", selectedUsername);
+//     // let usernames = ["standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user"]
+//     // let selectedUsername = usernames[0];
+//     // console.log("Selected username: ", selectedUsername);
 
 
-    //Validate login/validate title
-    cy.get('.app_logo').invoke('text').should('equal','Swag Labs');
+//     //Validate login/validate title
+//     cy.get('.app_logo').invoke('text').should('equal','Swag Labs');
 
     
-     //Select Filter Low to High
-     cy.get('.product_sort_container').select('Price (low to high)');
+//      //Select Filter Low to High
+//      cy.get('.product_sort_container').select('Price (low to high)');
 
-     //Validate the filter
-     cy.get('.select_container>span').should('text', 'Price (low to high)');
+//      //Validate the filter
+//      cy.get('.select_container>span').should('text', 'Price (low to high)');
  
-     //Add Product to cart 
-     cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click();
+//      //Add Product to cart 
+//      cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click();
  
-     //Navigate to Cart
-     cy.get('.shopping_cart_link').click();
+//      //Navigate to Cart
+//      cy.get('.shopping_cart_link').click();
  
-     //Validate the Product
-     cy.get('.cart_item_label').invoke('text').should('contains', 'Sauce Labs Bolt T-Shirt');
+//      //Validate the Product
+//      cy.get('.cart_item_label').invoke('text').should('contains', 'Sauce Labs Bolt T-Shirt');
  
-     //Remove the Product from the cart
-     cy.get('#remove-sauce-labs-bolt-t-shirt').click();
+//      //Remove the Product from the cart
+//      cy.get('#remove-sauce-labs-bolt-t-shirt').click();
  
-     //Validate that Product is removed or not 
-     cy.get('.cart_list>div').should('class', 'removed_cart_item');
+//      //Validate that Product is removed or not 
+//      cy.get('.cart_list>div').should('class', 'removed_cart_item');
  
  
-     //Another Way
-     cy.get('#continue-shopping').should('be.visible').click();
-     cy.get('.shopping_cart_link').click();
-     cy.get('.cart_list>div').should('have.length', 2);
-   });
+//      //Another Way
+//      cy.get('#continue-shopping').should('be.visible').click();
+//      cy.get('.shopping_cart_link').click();
+//      cy.get('.cart_list>div').should('have.length', 2);
+//    });
      
  
 
-  });
+//   });
     
 
--- cypress/e2e/inventory.cy.js
+++ cypress/e2e/inventory.cy.js
@@ -1,49 +1,49 @@
-import { loginTests } from './login.cy'
+// import { loginTests } from './login.cy'
 
-describe('inventory', () =>{
+// describe('inventory', () =>{
    
-    // before(() => {
-    //     loginTests() // Run this ONCE before all tests
-    //   })
+//     // before(() => {
+//     //     loginTests() // Run this ONCE before all tests
+//     //   })
 
-    it ('Product_search', () => {
+//     it ('Product_search', () => {
 
-        loginTests()
+//         loginTests()
 
-        //Validate login/validate title
-        cy.get('.app_logo').invoke('text').should('equal','Swag Labs');
+//         //Validate login/validate title
+//         cy.get('.app_logo').invoke('text').should('equal','Swag Labs');
             
-        //Select Filter Low to High
-        cy.get('.product_sort_container').select('Price (low to high)');
+//         //Select Filter Low to High
+//         cy.get('.product_sort_container').select('Price (low to high)');
 
-        //Validate the filter
-        cy.get('.select_container>span').should('text', 'Price (low to high)');
+//         //Validate the filter
+//         cy.get('.select_container>span').should('text', 'Price (low to high)');
 
-        //Add Product to cart 
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click();
+//         //Add Product to cart 
+//         cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click();
 
-        //Navigate to Cart
-        cy.get('.shopping_cart_link').click();
+//         //Navigate to Cart
+//         cy.get('.shopping_cart_link').click();
 
-        //Validate the Product
-        cy.get('.cart_item_label').invoke('text').should('contains', 'Sauce Labs Bolt T-Shirt');
+//         //Validate the Product
+//         cy.get('.cart_item_label').invoke('text').should('contains', 'Sauce Labs Bolt T-Shirt');
 
-        //Remove the Product from the cart
-        cy.get('#remove-sauce-labs-bolt-t-shirt').click();
+//         //Remove the Product from the cart
+//         cy.get('#remove-sauce-labs-bolt-t-shirt').click();
 
-        //Validate that Product is removed or not 
-        cy.get('.cart_list>div').should('class', 'removed_cart_item');
-
-
-        //Another Way
-        cy.get('#continue-shopping').should('be.visible').click();
-        cy.get('.shopping_cart_link').click();
-        cy.get('.cart_list>div').should('have.length', 2);
-
-});
+//         //Validate that Product is removed or not 
+//         cy.get('.cart_list>div').should('class', 'removed_cart_item');
 
 
-});
+//         //Another Way
+//         cy.get('#continue-shopping').should('be.visible').click();
+//         cy.get('.shopping_cart_link').click();
+//         cy.get('.cart_list>div').should('have.length', 2);
+
+// });
+
+
+// });
 
 
 
